8c52f07fe9e70b6878f74cb67f9f17920a8372db
TrazyJay <trazyjay@gmail.com>
2021-02-03 00:44:13 +0200

    Photo Album Project
    The start up of the photo album project

---
+++ MyPhotoGallery/MyPhotoGallery/Models/MyPhotosDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyPhotoGallery.Models
+{
+    public partial class MyPhotosDbContext : DbContext
+    {
+        public MyPhotosDbContext()
+        {
+        }
+
+        public MyPhotosDbContext(DbContextOptions<MyPhotosDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
